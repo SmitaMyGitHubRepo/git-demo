b71e34f2beef5292fa9a35b1c877ab7211fd1abd
SmitaMyGitHubRepo <sbdb551@gmail.com>
2022-09-29 18:05:35 -0400

    updated app java

--- src/main/java/com/SmitaTest/app/SmitaEclipseGitTest/App.java
+++ src/main/java/com/SmitaTest/app/SmitaEclipseGitTest/App.java
@@ -9,6 +9,10 @@ public class App
 	public void addData(String data)
 	{
 		System.out.println("test data="+data);
+		if (data.equals("Smita"))
+		{
+			System.out.println("Welcome");
+		}
 	}
     public static void main( String[] args )
     {
